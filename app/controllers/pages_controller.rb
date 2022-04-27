a853eaf7b5735e84cc4438462b1f687015a53933
anniegomezz <anniegomez@my.unt.edu>
2022-04-27 15:46:52 -0500

    intake_form added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,10 @@ class PagesController < ApplicationController
 	def landing
 	end
 	
+	def intake_form 
+		 @parts = Part.all
+		 @motherboard_parts = Part.where(part_type: "motherboard")
+		 @ram_parts = Part.where(part_type: "ram")
+		 @cheapparts = Part.where(:price == "33")
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'pages#landing'
-
+    root 'pages#landing'
+get 'pages/intake_form'
   get 'pages/about'
   get 'landing/about'
 end
