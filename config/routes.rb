fafe593e7b38bf9a15f0dbe995bfd5ffff5b6fa3
anniegomezz <anniegomez@my.unt.edu>
2022-04-27 14:14:35 -0500

    recipe video 5 and 6, navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#landing'
+
+  get 'pages/about'
+  get 'landing/about'
 end
